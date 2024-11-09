7e71f90d3ac88c7eca18fce357318667af74f827
p1petto <ksyuha3803@mail.ru>
2024-11-09 12:57:05 +0800

    добавлены полезные ресурсы

---
+++ Scripts/UsefulResources/resource_item.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+class_name ResourceItem
+
+@export var useful_resource: UsefulResource
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+@export var stacks: int = 1
+
+func _ready() -> void:
+	sprite_2d.texture = useful_resource.texture
+	collision_shape_2d.shape = useful_resource.ground_collision_shape
+
+func disable_collision():
+	collision_shape_2d.disabled = true
+	
+func enable_collision():
+	collision_shape_2d.disabled = false
---
+++ Scripts/UsefulResources/useful_resource.gd
@@ -0,0 +1,11 @@
+extends Resource
+
+class_name UsefulResource 
+
+@export var ground_collision_shape: CapsuleShape2D
+@export var name: String = ""
+@export var texture: Texture2D
+@export var side_texture: Texture2D
+@export var max_stacks: int
+@export var price: int
+@export var food: bool
