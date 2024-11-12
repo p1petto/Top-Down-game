8695cb975a04eb364d6263fde86c8f6681209863
p1petto <ksyuha3803@mail.ru>
2024-11-12 15:40:12 +0800

    новая сцена

--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -2,6 +2,14 @@ extends CharacterBody2D
 
 var player_is_near = false
 @onready var dialogue_system: Dialogue_system = $"../../UI/Dialogue"
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+@export var sprites: SpriteFrames
+
+func _ready() -> void:
+	if sprites:
+		animated_sprite.sprite_frames = sprites
+		animated_sprite.play("default")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == 'Player':
