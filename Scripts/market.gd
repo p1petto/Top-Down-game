19b027745228089d976e8138fcfd48ad7dd23c21
p1petto <ksyuha3803@mail.ru>
2024-11-13 22:59:13 +0800

    Основа маркета

---
+++ Scripts/market.gd
@@ -0,0 +1,81 @@
+extends Node
+
+class_name Market
+
+@onready var market_ui: MarketUI = $"../MarketUI"
+
+var items: Array[InventoryItem] = []
+#
+var taken_inventory_slots_count = 0
+#
+func _ready() -> void:
+	pass
+
+
+func add_item(item: InventoryItem, stacks: int):
+	#print_debug(item.name)
+	if stacks && item.max_stacks > 1:
+		add_stackable_item_to_inventory(item, stacks)
+	else:
+		#items.append(item)
+		var idx = items.find(null)
+		if idx != -1:
+			items[idx] = item
+		else: 
+			items.append(item)
+		market_ui.add_item(item)
+		taken_inventory_slots_count += 1
+	 #
+func add_stackable_item_to_inventory(item: InventoryItem, stacks: int):
+	
+	var empty_slot_index = items.find(null)
+	var existing_item_index = -1
+	
+	for i in items.size():
+		if items[i] != null and items[i].name == item.name:
+			existing_item_index = i
+			break
+	
+	if existing_item_index != -1:
+		var inventory_item = items[existing_item_index]
+		if inventory_item.stacks + stacks <= item.max_stacks:
+			inventory_item.stacks += stacks 
+			items[existing_item_index] = inventory_item
+			market_ui.update_stack_at_slot_index(inventory_item.stacks, existing_item_index)
+		else:
+			var stacks_diff = inventory_item.stacks + stacks - item.max_stacks
+			var additional_inventory_item = inventory_item.duplicate(true)
+			inventory_item.stacks = item.max_stacks
+			market_ui.update_stack_at_slot_index(inventory_item.max_stacks, existing_item_index)
+			
+			additional_inventory_item.stacks = stacks_diff
+			if empty_slot_index != -1:
+				items[empty_slot_index] = additional_inventory_item
+				market_ui.add_item(additional_inventory_item)
+			else:
+				items.append(additional_inventory_item)
+				market_ui.add_item(additional_inventory_item)
+			taken_inventory_slots_count += 1
+	else:
+		item.stacks = stacks
+		if empty_slot_index != -1:
+			items[empty_slot_index] = item
+			market_ui.add_item(item)
+		else:
+			items.append(item)
+			market_ui.add_item(item)
+		taken_inventory_slots_count += 1
+
+func on_item_equipped(idx: int, slot_to_equip: String):
+	var item_to_equip = items[idx]
+	print_debug(item_to_equip.name)
+
+
+func on_item_dropped(idx: int):
+	clear_inventory_slot(idx)
+	
+
+func clear_inventory_slot(idx: int):
+	taken_inventory_slots_count -= 1
+	market_ui.clear_slot_at_index(idx)
+	
--- Scripts/npc.gd
+++ Scripts/npc.gd
@@ -1,10 +1,22 @@
 extends CharacterBody2D
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var market: Market = $Market
+@onready var market_ui: MarketUI = $MarketUI
+
 @export var sprites: SpriteFrames
 @export var dialogue_resource: DialogueResource
+@export var items: Array[InventoryItem] = []
+
 var can_interact = false
 var is_dialogue_active = false
+
+func _ready() -> void:
+	DialogueManager.dialogue_ended.connect(on_dialogue_ended)
+	if len(items) > 0:
+		market.items = items.duplicate()
+		for item in items:
+			market_ui.add_item(item)
 
 func _input(event):
 	if event.is_action_pressed("interact") and can_interact and dialogue_resource and not is_dialogue_active:
@@ -12,10 +24,14 @@ func _input(event):
 
 func start_dialogue():
 	is_dialogue_active = true
-	DialogueManager.show_dialogue_balloon(dialogue_resource, "this_is_a_node_title")
+	DialogueManager.show_dialogue_balloon(dialogue_resource, "start")
+	
 
-func _on_dialogue_finished():
+func on_dialogue_ended(dialogue_resource):
 	is_dialogue_active = false
+	print ("finish")
+	if len(market.items) > 0:
+		market_ui.toggle()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == 'Player':
